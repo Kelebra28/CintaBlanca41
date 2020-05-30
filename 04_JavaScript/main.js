ecaf1bcfb35881d846fa77841b8596e0888cb87d
Kelebra <ricardokelebra@gmail.com>
2020-05-29 20:45:08 -0500

    Intro JavaScript

---
+++ 04_JavaScript/main.js
@@ -0,0 +1,35 @@
+//alert('Holaa')
+//Variables
+//Numericas
+var numero = 10;
+//String Texto
+var nombre = 'Adriana';
+//Booleano
+var verdadero = true ; //false
+//operadores aritmeticos +,-,*,/
+
+//Object o objeto
+var num1 = 200;
+var num2 = 300;
+var suma = num1 + num2;
+var saludo = 'Holaaa ';
+    // console.log() es que nos muestre que hace nuestro codigo en el navegador
+console.log(suma);
+console.log(nombre);
+console.log(verdadero);
+console.log('Hola desde la consola');
+//concatenar  es unir 2 o mas textos
+console.log(saludo  + nombre);
+
+// Es una ventana que nos permite ingresar datos de nuesto navegador
+var pregunta = prompt('Ingresa tu nombre');
+console.log(pregunta)
+
+
+// Challenge 1 
+// Preuntar al usuario su nombre, telefono y direccion y mostrarlo en la consola
+
+
+// Challenge 2
+// Preguntar al usuario su año de nacimiento y mostrar en consola que edad debe de tener este año
+
