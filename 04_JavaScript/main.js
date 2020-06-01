0609731d602b2ed4c37de726426eceaa50166f4c
Kelebra <ricardokelebra@gmail.com>
2020-06-01 18:27:10 -0500

    intro a JS

--- 04_JavaScript/main.js
+++ 04_JavaScript/main.js
@@ -1,29 +1,38 @@
-//alert('Holaa')
-//Variables
+// alert('Holaa')
+
+
+//Variables----->
+// var nombreVariable = valor
 //Numericas
 var numero = 10;
+
 //String Texto
 var nombre = 'Adriana';
+var numFalse = '10';
 //Booleano
 var verdadero = true ; //false
 //operadores aritmeticos +,-,*,/
 
-//Object o objeto
+
 var num1 = 200;
 var num2 = 300;
 var suma = num1 + num2;
+
+console.log(suma)
+
 var saludo = 'Holaaa ';
     // console.log() es que nos muestre que hace nuestro codigo en el navegador
-console.log(suma);
-console.log(nombre);
-console.log(verdadero);
-console.log('Hola desde la consola');
+// console.log(suma);
+// console.log(nombre);
+// console.log(verdadero);
+// console.log('Hola desde la consola');
 //concatenar  es unir 2 o mas textos
+// saludo + nombre
 console.log(saludo  + nombre);
 
 // Es una ventana que nos permite ingresar datos de nuesto navegador
-var pregunta = prompt('Ingresa tu nombre');
-console.log(pregunta)
+// var pregunta = prompt('Ingresa tu nombre');
+// console.log(pregunta)
 
 
 // Challenge 1 
@@ -33,3 +42,40 @@ console.log(pregunta)
 // Challenge 2
 // Preguntar al usuario su año de nacimiento y mostrar en consola que edad debe de tener este año
 
+
+
+// var pregunta = prompt('Ingresa tu nombre')
+// console.log(pregunta)
+// var pregunta2 = prompt('Ingresa tu telefono')
+// console.log(pregunta2)
+// var pregunta3 = prompt('Ingresa tu domicilio')
+// console.log(pregunta3)
+
+
+// var pregunta4 = prompt('Ingresa tu anio de nacimiento')
+// var anio = 2020
+// var edadReal = anio - pregunta4
+// console.log(edadReal)
+
+
+
+
+///------> Termina Challenge
+
+
+
+var edad = prompt('Ingresa año de nacimiento')
+console.log(edad) // '22'
+
+// typeof() -> tipo de valor que contine nuestra variable
+console.log(typeof(edad))
+
+// Number() canbia el valor de timpo texto o string a numero o number
+// parseInt()
+var edadNumero = Number(edad)
+console.log(edadNumero) //22
+
+console.log(typeof(edadNumero))
+
+
+// console.log()
