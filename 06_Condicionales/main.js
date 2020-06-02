cceb5cf7d64f0258d179d5f8c2d43d8b6e163df4
Kelebra <ricardokelebra@gmail.com>
2020-06-01 21:48:56 -0500

    condionales

--- 06_Condicionales/main.js
+++ 06_Condicionales/main.js
@@ -23,12 +23,30 @@ var num = 10
 
 
 if(num !==  10){
-    console.log('Es verdad')
+    // console.log('Es verdad')
 }else{
-    console.log('Es falso')
+    // console.log('Es falso')
 }
 
-// ------------------------- Challenge -------------------
+// ------------------------- Challenge 1-------------------
 // Investigar que es el MODULO -> % <-
 //Si un numero es par o impar 
 // P.d este numero lo tiene que dar el usiario.
+
+var preguntaNumero = prompt('Ingresa cualquier numero')
+var numReal = Number(preguntaNumero)
+
+if(numReal % 2 === 0){
+    console.log(numReal + ': es par')
+}else if(numReal % 2 === 1){
+    console.log(numReal + ': es impar' )
+}else{
+    console.error('Es un error, por favor checa tus datos')
+}
+
+
+
+// ------------------------- Challenge 2-------------------
+//Decir si el usuario puede pasar o no
+//Password : perritoSalchicha123
+
