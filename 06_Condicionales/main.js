b72d6ee9391aca13d70664629636679365aefc76
Kelebra <ricardokelebra@gmail.com>
2020-06-02 19:36:48 -0500

    condionales/operadores relacionales

--- 06_Condicionales/main.js
+++ 06_Condicionales/main.js
@@ -31,16 +31,16 @@ var num = 10
 //Si un numero es par o impar 
 // P.d este numero lo tiene que dar el usiario.
 
-var preguntaNumero = prompt('Ingresa cualquier numero')
-var numReal = Number(preguntaNumero)
+// var preguntaNumero = prompt('Ingresa cualquier numero')
+// var numReal = Number(preguntaNumero)
 
-if(numReal % 2 === 0){
-    console.log(numReal + ': es par')
-}else if(numReal % 2 === 1){
-    console.log(numReal + ': es impar' )
-}else{
-    console.error('Es un error, por favor checa tus datos')
-}
+// if(numReal % 2 === 0){
+//     console.log(numReal + ': es par')
+// }else if(numReal % 2 === 1){
+//     console.log(numReal + ': es impar' )
+// }else{
+//     console.error('Es un error, por favor checa tus datos')
+// }
 
 
 
@@ -48,10 +48,31 @@ if(numReal % 2 === 0){
 //Decir si el usuario puede pasar o no
 //Password : perritoSalchicha123
 
-var login = prompt('Ingresa tu contraseña')
+// var login = prompt('Ingresa tu contraseña')
 
-if(login === 'perritoSalchicha123'){
-    console.log('Bienvenido!')
+// if(login === 'perritoSalchicha123'){
+//     console.log('Bienvenido!')
+// }else{
+//     console.log('password incorrecta')
+// }
+
+//------------Operdadores Logicos-------
+
+//And && obliga a que las 2 condiciones sean true
+// OR \\ si uno se cumple nos arroja un true, si ninguno se cumple es false
+
+//  Edad para no conducir 0 ->15 y 71 ->
+// Edad para sacar persmiso 16 o 17
+// Edad para menejar 18 -> 70
+
+var edad = Number(prompt('Ingresa tu edad')) 
+
+if(edad >= 18 && edad <= 70 ){
+    console.log('Puedes sacar tu licencia')
+}else if(edad === 16 || edad === 17){
+    console.log('Puedes sacar tu permiso para conducir')
+}else if(edad <= 15 || edad >= 71){
+    console.log('No puedes conducir por tu seguridad y la de los demas')
 }else{
-    console.log('password incorrecta')
+    console.error('Datos incorrectos, intentalo de nuevo')
 }
