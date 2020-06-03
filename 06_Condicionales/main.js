6ab7885a2c11ae856d1fa3a477888af33072c3e3
Kelebra <ricardokelebra@gmail.com>
2020-06-02 21:05:02 -0500

    p,p,t complicado

--- 06_Condicionales/main.js
+++ 06_Condicionales/main.js
@@ -65,14 +65,69 @@ var num = 10
 // Edad para sacar persmiso 16 o 17
 // Edad para menejar 18 -> 70
 
-var edad = Number(prompt('Ingresa tu edad')) 
+// var edad = Number(prompt('Ingresa tu edad')) 
 
-if(edad >= 18 && edad <= 70 ){
-    console.log('Puedes sacar tu licencia')
-}else if(edad === 16 || edad === 17){
-    console.log('Puedes sacar tu permiso para conducir')
-}else if(edad <= 15 || edad >= 71){
-    console.log('No puedes conducir por tu seguridad y la de los demas')
+// if(edad >= 18 && edad <= 70 ){
+//     console.log('Puedes sacar tu licencia')
+// }else if(edad === 16 || edad === 17){
+//     console.log('Puedes sacar tu permiso para conducir')
+// }else if(edad <= 15 || edad >= 71){
+//     console.log('No puedes conducir por tu seguridad y la de los demas')
+// }else{
+//     console.error('Datos incorrectos, intentalo de nuevo')
+// }
+
+
+//---------------------- Piedra papel o tijera -------------
+// 2 jugadores  ----> propts
+//Consola mostrar que jugador gano.
+
+// ---> .toUpperCase()   -> todo a mayuscula
+// ---> .toLowerCase()  -> todo a minuscula
+
+
+// var player1 = prompt('Elige piedra, papel o tijera').toLowerCase()
+// var player2 = prompt('Elige piedra, papel o tijera').toLowerCase()
+// // console.log(player1)
+// console.log(player2)
+
+// if(player1 === player2){
+//     console.log('Empate')
+// }else if(player1 === 'papel' && player2 === 'piedra' || player1 === 'tijera' && player2 === 'papel' || player1 === 'piedra' && player2 === 'tijera' ){
+//     console.log('Gana player 1')
+// }else if(player2 === 'papel' && player1 === 'piedra' || player2 === 'tijera' && player1 === 'papel' || player2 === 'piedra' && player1 === 'tijera'){
+//     console.log('Gana jugador 2')
+// }else{
+//     console.error('Algo no salio como se esperaba')
+// }
+
+//---------- 💩 PPT Complicado 💩 -------- >
+
+var userAnswer = prompt('Ingresa Piedra, Papel o Tijera').toLowerCase()
+
+//PC
+var pcAnswer = ''
+
+// Math es el metodo de js para usar funciones matematicas
+// .floor --> regresa el minimo o el maxio ----> (0, 5) regresa *0, 1, 2, 3, 4, 5 *
+let options = Math.floor((Math.random() * 3) + 1 )
+
+if(options === 1){ pcAnswer = 'piedra' }
+if(options === 2) { pcAnswer = 'papel' }
+if(options === 3) { pcAnswer = 'tijera' } 
+
+if(userAnswer === pcAnswer){
+    console.log('Es un empate porque: ')
+    console.log('User: ' + userAnswer)
+    console.log('PC: ' + pcAnswer)
+}else if(pcAnswer === 'papel' && userAnswer === 'piedra' || pcAnswer === 'tijera' && userAnswer === 'papel' || pcAnswer === 'piedra' && userAnswer === 'tijera' ){
+    console.log('Gana la PC porque :')
+    console.log('User: ' + userAnswer)
+    console.log('PC: ' + pcAnswer)
+}else if(userAnswer === 'papel' && pcAnswer === 'piedra' || userAnswer === 'tijera' && pcAnswer === 'papel' || userAnswer === 'piedra' && pcAnswer === 'tijera'){
+    console.log('Gana la User porque :')
+    console.log('User: ' + userAnswer)
+    console.log('PC: ' + pcAnswer)
 }else{
-    console.error('Datos incorrectos, intentalo de nuevo')
+    console.error('Ingresa bien tus opciones')
 }
