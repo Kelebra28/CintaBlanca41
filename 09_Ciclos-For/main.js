7c8b617370219dce3c31ffe5e9dfa10f6d79b15a
Kelebra <ricardokelebra@gmail.com>
2020-06-08 21:38:20 -0500

    Funciones

--- 09_Ciclos-For/main.js
+++ 09_Ciclos-For/main.js
@@ -8,9 +8,9 @@
 //     instruccion
 // }
 
-for(var i = 0;  i <= 10; i = i + 1){
-    // console.log(i)
-}
+// for(var i = 0;  i <= 10; i = i + 1){
+//     // console.log(i)
+// }
 
 // Empiece en 0  su  - 200 limite y la secuencia  5
 // for(var e = 0;  e >= -200; e = e -5){
@@ -31,7 +31,40 @@ for(var i = 0;  i <= 10; i = i + 1){
 // }
 
 
+//  FizzzBuuz
+
+// --------------------------Challege Cool-----------------
+// Del 1 al 100
+// Si el numero es divisible entre 3 ----> Fizz
+// Si el numero es divisible entre 5 ----> Buzz
+// Si el numeor es divisible entre 3 y 5 ---> FizzBuzz
+
+// Ejemplo: 
+// 1 
+// 2
+// 3 Fizz
+// 4
+// 5 Buzz
+// ...
+// 15 FizzBuzz
+// 16
+
+//Errores
+
+// 15 fizz
+// 15 buzz
+// 15 fizzBuzz
 
 
 
-
+for(let y = 0; y <= 100; y++){
+    if(y % 3 === 0 && y % 5 === 0){
+        console.log(y + ': FizzBuzz')
+    }else if(y % 5 === 0){
+        console.log(y + ' : Buzz')
+    }else if( y % 3 === 0){
+        console.log(y + ' : Fizz')
+    }else{
+        console.log(y)
+    }
+}
---
+++ 10_Funciones/main.js
@@ -0,0 +1,41 @@
+
+//Funciones
+
+//Vanilla js
+// var num1 = 10
+// var num1  = 'fhiuafsd'
+
+//ECMA 5
+// let num2 = 10 
+//     num2 = 'ghjsdlkgd'
+
+// vanillajs
+// function saludo (){
+
+//     console.log('hola Ricardo')
+// }
+
+// saludo()
+
+//ECMA 5 Arrow fuction
+const saludar2 = (nombre, edad) => {
+
+        console.log("Hola " + nombre + ' y tienes la edad de: ' + edad)
+}
+
+ //        nombre, edad   
+saludar2('Ricardo', 22)
+saludar2('Oliver', 27)
+saludar2('Dana', 29)
+
+
+const sum = (x, y) => {
+    console.log(x + y)
+}
+
+sum(50, 100)
+
+
+//Van hacer una funcion que nos de el perimetro de un cuadrado
+// Area de un reactangulo
+// Area de un triangulo
