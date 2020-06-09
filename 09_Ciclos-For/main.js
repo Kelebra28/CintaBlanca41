318d196db80aebe90157a51a7aaf0e82631e4bc0
Kelebra <ricardokelebra@gmail.com>
2020-06-08 19:38:13 -0500

    ciclo for

---
+++ 09_Ciclos-For/main.js
@@ -0,0 +1,18 @@
+
+
+
+
+//Estructura
+
+// for(inicio; limite; secuencia){
+//     instruccion
+// }
+
+for(var i = 0;  i <= 10; i = i + 1){
+    console.log(i)
+}
+
+// Empiece en 0  su  - 200 limite y la secuencia  5
+// Empiece en 500  su limite 0 y la secuencia 10
+// Empiece en -500  su limite 0 y la secuencia 50
+
