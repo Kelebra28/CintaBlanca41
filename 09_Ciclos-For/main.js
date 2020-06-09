e1d55c38bf2c7379ba744fa2f042e825bf10afae
Kelebra <ricardokelebra@gmail.com>
2020-06-08 20:02:48 -0500

    ciclo for

--- 09_Ciclos-For/main.js
+++ 09_Ciclos-For/main.js
@@ -9,10 +9,29 @@
 // }
 
 for(var i = 0;  i <= 10; i = i + 1){
-    console.log(i)
+    // console.log(i)
 }
 
 // Empiece en 0  su  - 200 limite y la secuencia  5
-// Empiece en 500  su limite 0 y la secuencia 10
+// for(var e = 0;  e >= -200; e = e -5){
+//     console.log(e)
+// }
+
+
+// Empeece en 500  su limite 0 y la secuencia 10
+
+// for(var a = 500;  a >= 0; a = a - 10){
+//     console.log(a)
+// }
+
 // Empiece en -500  su limite 0 y la secuencia 50
+
+// for(var x = -500;  x <= 0 ; x = x +50 ){
+//     console.log(x)
+// }
+
+
+
+
+
 
