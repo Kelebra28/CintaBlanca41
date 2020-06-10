49157297bbd15aafd5df31e221a451c1822b916b
Kelebra <ricardokelebra@gmail.com>
2020-06-09 21:36:57 -0500

    HTML + JS

--- 11_DOM/Challenge/main.js
+++ 11_DOM/Challenge/main.js
@@ -4,3 +4,36 @@
 // -Direccion
 // -Telefono
 // -Pinia o sin pinia
+//Inputs
+let Pnombre = document.getElementById('Pnombre')   
+let Pdireccion = document.getElementById('Pdireccion')   
+let Ptelefono = document.getElementById('Ptelefono')   
+let Ppinia = document.getElementById('Ppinia')   
+
+// p
+let Rnombre = document.getElementById('Rnombre')   
+let Rdireccion = document.getElementById('Rdireccion')   
+let Rtelefono = document.getElementById('Rtelefono')   
+let Rpinia = document.getElementById('Rpinia')   
+
+//CARD
+let card = document.getElementById('card')
+
+//button
+let boton = document.getElementById('boton')   
+
+const ordenar = () => {
+
+    Rnombre.innerHTML = Pnombre.value
+    Rdireccion.innerHTML = Pdireccion.value
+    Rtelefono.innerHTML = Ptelefono.value
+    Rpinia.innerHTML = Ppinia.value
+
+    // classList me permite acceder a todas las clases de mi etiqueda.
+    // console.log(card.classList)
+    card.classList.remove('none')
+    
+
+}
+
+boton.addEventListener('click', ordenar)
