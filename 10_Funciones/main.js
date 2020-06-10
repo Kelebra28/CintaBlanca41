2a25694c6e967c07fb71dd1b18226c39e7aeb699
Kelebra <ricardokelebra@gmail.com>
2020-06-09 19:30:18 -0500

    Funciones

--- 10_Funciones/main.js
+++ 10_Funciones/main.js
@@ -24,18 +24,49 @@ const saludar2 = (nombre, edad) => {
 }
 
  //        nombre, edad   
-saludar2('Ricardo', 22)
-saludar2('Oliver', 27)
-saludar2('Dana', 29)
+// saludar2('Ricardo', 22)
+// saludar2('Oliver', 27)
+// saludar2('Dana', 29)
 
 
 const sum = (x, y) => {
-    console.log(x + y)
+    
+   return x + y
+
 }
 
-sum(50, 100)
+// sum(50, 100)
+// console.log(sum(10,15))
+
+//funcion como parametro
+// console.log( sum(sum(50, 5), 10))
 
 
 //Van hacer una funcion que nos de el perimetro de un cuadrado
+
+const perimetro = (l) => {
+       return l * 4
+} 
+
+// console.log(perimetro(4))
 // Area de un reactangulo
+
+const areaRectangulo = (b, h) => {
+    // console.log(b)
+
+   return b * h
+}
+// console.log(b)
+console.log(areaRectangulo(10,5))
+
 // Area de un triangulo
+
+const areaTriangulo = (b, h) => {
+
+    return (b * h)/ 2
+}
+
+// console.log(areaTriangulo(50, 60))
+
+console.log(sum(areaRectangulo(10, 5), areaTriangulo(50, 60)))
+
