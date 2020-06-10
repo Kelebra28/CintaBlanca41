a8cd2929aeb981d5a52af3d387187c3841c5f56b
Kelebra <ricardokelebra@gmail.com>
2020-06-09 20:09:38 -0500

    HTML + JS

--- 11_DOM/main.js
+++ 11_DOM/main.js
@@ -1,10 +1,13 @@
 // getElementById busca el id en nuestro archivo HTML
 let titulo = document.getElementById('titulo')
 let texto = document.getElementById('texto')
+let pregunta = document.getElementById('pregunta')
+let respuesta = document.getElementById('respuesta')
 let boton = document.getElementById('boton')
 
-console.log(titulo)
-console.log(texto)
+// console.log(pregunta)
+// console.log(titulo)
+// console.log(texto)
 
 const cambio = () => {
 
@@ -12,10 +15,14 @@ const cambio = () => {
 titulo.innerHTML = 'Titulo cambiado desde JS'
 texto.innerHTML = 'Texto cambiado desde JS'
 
+
+// .value es el valor del input, en otras palabras lo que el usuario ingresa
+    console.log(pregunta.value)
+    respuesta.innerHTML = pregunta.value
+
 }
 // addEventListener escucha los eventos de HTML
 //El primer parametro es el evento
 //Segundo parameto es la funcion que queremos ejecutar
-
 boton.addEventListener('click', cambio)
 
